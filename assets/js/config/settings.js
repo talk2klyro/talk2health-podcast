4e30cf398f1d3e2a68ea08d7495f6570ec6d94d3
talk2klyro <talk2klyro@gmail.com>
2025-10-19 06:34:33 +0100

    Add configuration settings for payment and forms

---
+++ assets/js/config/settings.js
@@ -0,0 +1,7 @@
+export const CONFIG = {
+  PAYSTACK_PUBLIC_KEY: 'pk_test_replace_with_yours',
+  PAYSTACK_DEFAULT_EMAIL: 'student@example.com',
+  JOTFORM_FORM_ID: 'YOUR_FORM_ID',
+  JOTFORM_BASE: 'https://form.jotform.com',
+  GOOGLE_CLASSROOM_SHARE_BASE: 'https://classroom.google.com/u/0/share?url='
+};
